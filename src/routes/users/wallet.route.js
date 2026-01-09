96ad2fb20edf5035a09811996c331898e112dd15
Adeel Niazi <adeek.ak1999@gmail.com>
2026-01-10 00:56:35 +0500

    fix: typo errors

--- src/routes/users/wallet.route.js
+++ src/routes/users/wallet.route.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const WalletController = require("../../controllers/WalletController");
+const WalletController = require("../../controllers/wallet.controller");
 const { authenticateJwt } = require("../../middlewares/auth.middleware");
 
-router.get("/history", authenticateJwt, WalletController.getHistory);
-router.get("/balance", authenticateJwt, WalletController.getBalance);
+router.get("/history", authenticateJwt, WalletController.getHistory.bind(WalletController));
+router.get("/balance", authenticateJwt, WalletController.getBalance.bind(WalletController));
 
 module.exports = router;
 
